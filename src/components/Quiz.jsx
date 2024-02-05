409f943c7ea500caf4781c432471a290be55b091
Michael Emad <michaelebeid9@gamil.com>
2024-02-05 18:02:33 +0200

    Moving Logic To Components That Actually Need It (Moving State Down)

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -4,32 +4,17 @@ import quizCompleteImg from "../assets/quiz-complete.png";
 import Question from "./Question.jsx";
 
 export default function Quiz() {
-  const [answerState, setAnswerState] = useState("");
   const [userAnswers, setUserAnswers] = useState([]);
 
-  const activeQuestionIndex =
-    answerState === "" ? userAnswers.length : userAnswers.length - 1;
+  const activeQuestionIndex = userAnswers.length;
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   const handleSelectAnswer = useCallback(
     function handleSelectAnswer(selectedAnswer) {
-      setAnswerState("answered");
       setUserAnswers((prevAnswer) => {
         return [...prevAnswer, selectedAnswer];
       });
-      setTimeout(() => {
-        if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
-          setAnswerState("correct");
-        } else {
-          setAnswerState("wrong");
-        }
-        setTimeout(() => {
-          setAnswerState("");
-        }, 2000);
-      }, 1000);
-    },
-    [activeQuestionIndex]
-  );
+    },[]);
 
   const handleSkipAnswer = useCallback(
     () => handleSelectAnswer(null),
@@ -49,10 +34,7 @@ export default function Quiz() {
     <div id="quiz">
       <Question
         key={activeQuestionIndex}
-        answers={QUESTIONS[activeQuestionIndex].answers}
-        questionText={QUESTIONS[activeQuestionIndex].text}
-        selectedAnswer={userAnswers[userAnswers.length - 1]}
-        answerState={answerState}
+        index={activeQuestionIndex}
         onSkipAnswer={handleSkipAnswer}
         onSelectAnswer={handleSelectAnswer}
       />
